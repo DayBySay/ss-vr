23c747a0c705605f85d6d7649b8d7e2b9e079de4
Takayuki Sei <takayuki_sei@voyagegroup.com>
2017-01-14 10:59:58 +0900

    タイポ

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import aframe from 'afram'
+import aframe from 'aframe'
 import socketio from 'socket.io-client'
 import registerVideoBillboard from 'aframe-video-billboard';
 registerVideoBillboard(aframe);
